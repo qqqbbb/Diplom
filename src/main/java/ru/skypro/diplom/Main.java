4e60a8f42b91d40b3096e019be3933d96aa92cd9
qqqbbb <qqqinbox@ya.ru>
2023-03-22 16:43:01 +0300

    dev test1

--- src/main/java/ru/skypro/diplom/Main.java
+++ src/main/java/ru/skypro/diplom/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-        LoggerFactory.getLogger(Main.class).info("!  started  !");
+        LoggerFactory.getLogger(Main.class).info("!  started  !!!");
     }
 
 }
